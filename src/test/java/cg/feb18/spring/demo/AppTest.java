19940c63d25bf85031db015b8ea79877cabe340d
vamandeshmukh <vamandeshmukh@gmail.com>
2022-03-23 11:20:03 +0530

    sample JUnit test

--- src/test/java/cg/feb18/spring/demo/AppTest.java
+++ src/test/java/cg/feb18/spring/demo/AppTest.java
@@ -1,8 +1,18 @@
 package cg.feb18.spring.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+
+	@Test
+	public void testTest() {
+		System.out.println("testTest");
+		assertEquals(10, 10);
+	}
 
 }
