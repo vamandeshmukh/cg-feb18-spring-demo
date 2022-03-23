2b02395fac1e6342ee284574990de7390b27c940
vamandeshmukh <vamandeshmukh@gmail.com>
2022-03-23 10:50:22 +0530

    Commit
    Commit

---
+++ src/main/java/cg/feb18/spring/demo/App.java
@@ -0,0 +1,13 @@
+package cg.feb18.spring.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
